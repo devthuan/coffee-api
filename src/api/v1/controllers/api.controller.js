4597af5ec9fbcc454c88645b4ee004ed4f4bb862
devthuan <devthuan24@gmail.com>
2023-09-07 22:01:11 +0700

    connect mysql

--- src/api/v1/controllers/api.controller.js
+++ src/api/v1/controllers/api.controller.js
@@ -1,7 +1,27 @@
-const HomePages = (req, res, next) => {
-  res.json({
-    message: "Home PAGE",
-  });
+const { connection } = require("../../../config/index.config");
+
+const HomePages = async (req, res, next) => {
+  try {
+    connection.query("select * from users", (error, result, fields) => {
+      if (error) {
+        console.log(error);
+        res.status(500).json({
+          error: "Database error",
+        });
+        return;
+      }
+
+      res.json({
+        data: result,
+      });
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      error: "Error",
+    });
+    return;
+  }
 };
 
 module.exports = {
